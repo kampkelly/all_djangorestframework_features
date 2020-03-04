9fd26711dcdd2c258019106cbf5d205b1a25225f
kampkelly <kampkellykeys@gmail.com>
2020-03-04 22:18:02 +0100

    add basic function views and serializer

--- notesapp/api_v1/urls.py
+++ notesapp/api_v1/urls.py
@@ -4,10 +4,15 @@ from .views import Category
 
 app_name = 'api_v1'
 
+# function based views
+list_categories = Category.CategoryFunctionView.list_categories
+
+# class based views
 categories = Category.CategoryView.as_view({
     'get': 'list'
 })
 
 urlpatterns = [
-    path('categories', categories, name='categories')
+    path('categories', categories, name='categories'),
+    path('fn/categories', list_categories, name='categories')
 ]
--- notesapp/api_v1/views/Category.py
+++ notesapp/api_v1/views/Category.py
@@ -1,13 +1,23 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from rest_framework import status
 
+
 from api_v1.models import Category
-# from api.serializers import CategorySerializer, save_serializer
+from api_v1.serializers import CategorySerializer
 
 
 class CategoryView(ViewSet):
     def list(self, request):
         categories = Category.objects.all()
-        # serializer = CategorySerializer(categories, many=True)
-        return Response(categories, status=status.HTTP_200_OK)
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CategoryFunctionView:
+    @api_view(['GET'])
+    def list_categories(request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
