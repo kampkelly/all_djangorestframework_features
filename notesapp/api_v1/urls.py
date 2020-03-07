48423a95ecd13e9741504a355f3bdffd4eb99ee7
kampkelly <kampkellykeys@gmail.com>
2020-03-07 23:58:15 +0100

    implement routers
    view names: list, create, retrieve, update, partial_update and destroy

--- notesapp/api_v1/urls.py
+++ notesapp/api_v1/urls.py
@@ -1,20 +1,19 @@
 from django.urls import path
+from rest_framework import routers
 
 from .views import Category
 
 app_name = 'api_v1'
 
+router = routers.SimpleRouter()
+
 # function based views
 list_categories = Category.CategoryFunctionView.list_categories
 
-# class based views
-categories = Category.CategoryView.as_view({
-    'get': 'list'
-})
+router.register(r'categories', Category.CategoryView, 'categories')
 
 urlpatterns = [
-    # class based views urls
-    path('categories', categories, name='categories'),
     # function based views urls
     path('fn/categories', list_categories, name='categories')
 ]
+urlpatterns += router.urls
--- notesapp/api_v1/views/Category.py
+++ notesapp/api_v1/views/Category.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.exceptions import APIException
+from rest_framework.decorators import action
 
 from api_v1.models import Category
 from api_v1.serializers import CategorySerializer
@@ -16,6 +17,22 @@ class WrongVersion(APIException):
 
 class CategoryView(ViewSet):
     def list(self, request, format=None):
+        '''
+        Lists all categories.
+        '''
+        if request.version == 'api_v1':
+            categories = Category.objects.all()
+            serializer = CategorySerializer(categories, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            WrongVersion.default_detail = 'Accessing wrong api version, use api_v1'
+            raise WrongVersion
+
+    @action(methods=['get'], detail=False, url_path='categories-list', url_name='my-lists')
+    def categories_list(self, request, pk=None, format=None):
+        '''
+        This method is just to demo router @action.
+        '''
         if request.version == 'api_v1':
             categories = Category.objects.all()
             serializer = CategorySerializer(categories, many=True)
@@ -28,6 +45,9 @@ class CategoryView(ViewSet):
 class CategoryFunctionView:
     @api_view(['GET'])
     def list_categories(request, format=None):
+        '''
+        Lists all categories.
+        '''
         if request.version == 'api_v2':
             categories = Category.objects.all()
             serializer = CategorySerializer(categories, many=True)
